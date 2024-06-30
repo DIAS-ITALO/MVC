2b4d0a4d84b4f1c281b027bed8da00a04cdbac74
Italo-Diass <italogabrielsiqueira02@gmail.com>
2024-06-29 21:08:02 -0300

    10° feat

--- src/main/constructor/people_finder_constructor.py
+++ src/main/constructor/people_finder_constructor.py
@@ -1,7 +1,15 @@
 from src.people_finder_view import PeopleFinderView
+from src.controlles.people_finder_controller import PeopleFinderController
 
 def people_finder_contructor():
     people_finder_view = PeopleFinderView()
-    #controller   
+    people_finder_controller = PeopleFinderController
+    
+
     person_finder_informations = people_finder_view.find_person_view()
-    #enviar para controller
+    response = person_finder_controller.find_by_name(person_finder_informations)
+    
+    if response["sucess"]:
+        people_finder_view.find_person_sucess(response["message"])
+    else:
+        people_finder_view.finde_person_fail(response["error"])
