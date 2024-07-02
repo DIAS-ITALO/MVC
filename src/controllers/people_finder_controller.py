fcfcb6a9c103ad7d992179cd13f8ea2d94d16abf
Italo-Diass <italogabrielsiqueira02@gmail.com>
2024-07-01 22:34:46 -0300

    13° feat

--- src/controllers/people_finder_controller.py
+++ src/controllers/people_finder_controller.py
@@ -1,11 +1,13 @@
 from typing import Dict
+from src.models.repository.person_repository import person_repository
+from src.models.entities.person import Person
 
-class PeopleFinderCOntroller:
+class PeopleFinderController:
     def find_by_name(self, person_finder_informations: Dict) -> Dict:
         try:
             self.__validate_fields(person_finder_informations)
-            #person = Buscar em banco de dados
-            response = self.__format_response(None)
+            person = self.__find_person(person_finder_informations)
+            response = self.__format_response(person)
             return {"sucess": True, "message": response}
         except Exception as exception:
             pass
@@ -13,6 +15,15 @@ class PeopleFinderCOntroller:
     def __validate_fields(self, person_finder_informations: Dict) -> None:
         if not isinstance(person_finder_informations["name"], str):
             raise Exception('Campo Nome Invalido')
+        
+    def __find_person(self, person_finder_informations: Dict):
+        name = person_finder_informations["name"]
+        
+        person = person_repository.find_person_by_name(name)
+        if not person: raise Exception('Pessoa não encontrada!')
+        
+        return person
+        
         
     def __format_response(self, person: any) -> Dict:
         return {
